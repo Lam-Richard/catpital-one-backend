981bd48072a6cbefe40fb769872b54765094a4b3
Lam-Richard <richardlam2024@gmail.com>
2021-08-12 17:08:57 -0500

    generic error catching

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const jsonParser = bodyParser.json();
 
 const Alpaca = require('@alpacahq/alpaca-trade-api');
 const alpaca = new Alpaca({
-    keyId: 'INSERTKEYHERE',
-    secretKey: 'INSERTKEYHERE',
+    keyId: 'KEY',
+    secretKey: 'OTHERKEY',
     paper: true,
     usePolygon: false
 });
@@ -15,28 +15,33 @@ const app = express();
 const port = 3000;
 
 
-//
+
 app.get('/', (req, res) => {
     console.log('connected');
     res.send('connected');
 })
 
-//
+// Gets account info
 app.get('/account', (req, res) => {
     alpaca.getAccount().then((account) => {
         res.send(account);
+    }).catch(e => {
+        res.send(e);
     })
 });
 
-//
+// Gets information about a specific asset
 app.get('/asset/:symbol', (req, res) => {
+
     alpaca.getAsset(req.params.symbol).then(data => {
         res.send(data);
+    }).catch(e => {
+        res.send(e);
     })
+   
 })
 
-
-//
+// Place a buy or sell order. Must provide symbol, quantity (shares) and 'buy' or 'sell'
 app.post('/buySell', jsonParser,  (req, res) => {
     alpaca.createOrder({
         symbol: req.body.symbol, 
@@ -46,33 +51,48 @@ app.post('/buySell', jsonParser,  (req, res) => {
         time_in_force: 'day',
     }).then(data => {
         res.send(data);
+    }).catch(e => {
+        res.send(e);
     })
 })
 
+// Gets a list of the user's positions
 app.get('/positions', (req, res) => {
     alpaca.getPositions().then(data => {
         res.send(data);
+    }).catch(e => {
+        res.send(e);
     })
 })
 
+// Gets the user's position for a specific stock
 app.get('/position/:symbol', (req, res) => {
     alpaca.getPosition(req.params.symbol).then(data => {
         res.send(data);
+    }).catch(e => {
+        res.send(e);
     })
 })
 
+// Gets a list of all assets (so what's tradeable w the api, not what's owned)
 app.get('/allAssets', (req, res) => {
     alpaca.getAssets().then(data => {
         res.send(data);
+    }).catch(e => {
+        res.send(e);
     })
 })
 
-
+// Gets a list of all orders previously placed
 app.get('/orders', (req, res) => {
     alpaca.getOrders().then(data => {
         res.send(data);
+    }).catch(e => {
+        res.send(e);
     })
 })
+
+
 
 
 app.listen(port, () => {
